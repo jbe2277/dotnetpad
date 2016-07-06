a339c953d0f10370b620f8479d24a854dc768b2b
jbe2277 <jbe2277@users.noreply.github.com>
2016-07-06 22:50:09 +0200

    Prepare release 2.0.1.100

--- src/DotNetPad/SharedAssemblyInfo.cs
+++ src/DotNetPad/SharedAssemblyInfo.cs
@@ -17,5 +17,5 @@ using System.Resources;
 [assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.MainAssembly)]
 
 
-[assembly: AssemblyVersion("2.0.0.100")]
-[assembly: AssemblyFileVersion("2.0.0.100")]
+[assembly: AssemblyVersion("2.0.1.100")]
+[assembly: AssemblyFileVersion("2.0.1.100")]
