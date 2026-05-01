5336709f6cabf70374bb0d19223e3bc57fea172c
jbe2277 <jbe2277@users.noreply.github.com>
2026-05-01 23:26:49 +0200

    Fix PerformanceTrace and improve timing precision

--- src/DotNetPad/DotNetPad.Domain/PerformanceTrace.cs
+++ src/DotNetPad/DotNetPad.Domain/PerformanceTrace.cs
@@ -2,14 +2,18 @@
 
 namespace Waf.DotNetPad.Domain;
 
-public sealed class PerformanceTrace(string name) : IDisposable
+public sealed class PerformanceTrace : IDisposable
 {
     private readonly long start;
+    private readonly string name;
 
-    public PerformanceTrace(string name, DocumentFile document) : this(document.FileName + ": " + name)
+    public PerformanceTrace(string name)
     {
+        this.name = name;
         start = Stopwatch.GetTimestamp();
     }
 
-    public void Dispose() => Log.Default.Trace(">>> {0}: {1} ms", name, Stopwatch.GetElapsedTime(start));
+    public PerformanceTrace(string name, DocumentFile document) : this(document.FileName + ": " + name) {         }
+
+    public void Dispose() => Log.Default.Trace(">>> {0}: {1} ms", name, Stopwatch.GetElapsedTime(start).TotalMilliseconds);
 }
